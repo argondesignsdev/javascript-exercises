a7aab1b74486844ae6ad31d9c91fb5b64363499c
Ruben Gonzales <argondesignsdev@gmail.com>
2022-09-26 00:30:52 -0700

    working on exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+const strArr = [];
+let reversed = [];
+counter = 0;
+for (let i = 0; i < str.length; i++){
+  strArr.push(str[i]);
+}
 
+for (let i = strArr.lengthl; i > 0; i--){
+counter++;
+reversed[counter] = strArr[i]
+}
+console.log(reversed)
 };
 
 // Do not edit below this line
