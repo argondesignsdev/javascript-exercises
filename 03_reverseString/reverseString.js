4ccb26090dbc64ce9eedd9bc653f6631fdbdf973
Ruben Gonzales <argondesignsdev@gmail.com>
2022-09-26 11:00:59 -0700

    work

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,16 +1,10 @@
 const reverseString = function(str) {
-const strArr = [];
-let reversed = [];
-counter = 0;
-for (let i = 0; i < str.length; i++){
-  strArr.push(str[i]);
+  const toArray =[]
+for (i = str.length; i >= 0; i--) {
+  toArray.push(str[i])
 }
-
-for (let i = strArr.lengthl; i > 0; i--){
-counter++;
-reversed[counter] = strArr[i]
-}
-console.log(reversed)
+const reverse = toArray.join('')
+return reverse;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rem) {
+const newArrr = []
+// Loop through arr 
+for (el of arr){
+  if(!arr[el].in)
+}
+// for every index that doesn't include values from rem add to new arr
 };
 
 // Do not edit below this line
